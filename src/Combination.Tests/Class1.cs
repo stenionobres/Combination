5e0beae19b422c446b3c126c1ba8630f0e964906
Stenio Nobres Batista <stenio.nobres@gmail.com>
2022-09-04 09:28:18 -0300

    Adds Combination.Tests project

---
+++ src/Combination.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Combination.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.AreEqual(4, 4);
+        }
+    }
+}
