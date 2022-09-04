77d942c35d10916866b8ddd656dc888499c6dfc1
Stenio Nobres Batista <stenio.nobres@gmail.com>
2022-09-04 18:29:41 -0300

    Adds CSN initial class and unit tests

---
+++ src/Combination.Core/CSN.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Combination.Core
+{
+    public class CSN
+    {
+        public static int Calculate(int totalElements, List<int> combination)
+        {
+            return 1;
+        }
+    }
+}
---
+++ src/Combination.Tests/CSNTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Combination.Core;
+using System.Collections.Generic;
+
+namespace Combination.Tests
+{
+    [TestFixture]
+    public class CSNTests
+    {
+        [Test]
+        public void GivenFiveElementsAndCombinationWithThreeNumbers_WhenCSNIsCalculated_ThenNumberTwoIsGenerated()
+        {
+            var totalElements = 5;
+            var combination = new List<int>() { 1, 2, 3 };
+            var csnExpected = 1;
+            var csnCalculated = CSN.Calculate(totalElements, combination);
+
+            Assert.AreEqual(csnExpected, csnCalculated);
+        }
+    }
+}
