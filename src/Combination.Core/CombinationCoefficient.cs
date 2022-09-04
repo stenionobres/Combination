ea5e0fcd5755a042ab059721feb3018f885b7beb
Stenio Nobres Batista <stenio.nobres@gmail.com>
2022-09-04 11:15:28 -0300

    Adds CombinationCoefficient initial class

---
+++ src/Combination.Core/CombinationCoefficient.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Combination.Core
+{
+    public class CombinationCoefficient
+    {
+        public static int Calculate(int totalElements, int combinationSize)
+        {
+            return 1;
+        }
+    }
+}
---
+++ src/Combination.Tests/CombinationCoefficientTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Combination.Core;
+
+namespace Combination.Tests
+{
+    [TestFixture]
+    public class CombinationCoefficientTest
+    {
+        [Test]
+        public void GivenOneElementAndOneCombinationSize_WhenCombinationCoefficientIsCalculated_ThenNumberOneIsGenerated()
+        {
+            var totalElements = 1;
+            var combinationSize = 1;
+            var combinationCoefficientExpected = 1;
+            var combinationCoefficientCalculated = CombinationCoefficient.Calculate(totalElements, combinationSize);
+
+            Assert.AreEqual(combinationCoefficientExpected, combinationCoefficientCalculated);
+        }
+    }
+}
