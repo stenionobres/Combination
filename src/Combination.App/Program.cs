dbf05987739ae33638a325b1a53afd04bf8a74cf
Stenio Nobres Batista <stenio.nobres@gmail.com>
2022-09-04 08:54:39 -0300

    Adds Combination.App

---
+++ src/Combination.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Combination.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
