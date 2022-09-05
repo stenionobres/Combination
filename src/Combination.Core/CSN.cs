7a2f9bd2738fdc122131c3b0b73bea6a93c084c4
Stenio Nobres Batista <stenio.nobres@gmail.com>
2022-09-05 19:30:58 -0300

    Remove extra index from NUM variable

--- src/Combination.Core/CSN.cs
+++ src/Combination.Core/CSN.cs
@@ -6,14 +6,14 @@ namespace Combination.Core
     {
         public static int Calculate(int totalElements, List<int> combination)
         {
-            var NUM = new int[combination.Count + 1];
-            var C = new int[combination.Count + 1];
+            var NUM = new int[combination.Count];
+            var C = new int[combination.Count];
             var LI = 0;
             var CSN = 0;
 
-            for (int i = 1; i <= combination.Count; i++)
+            for (int i = 0; i < combination.Count; i++)
             {
-                NUM[i] = combination[i - 1];
+                NUM[i] = combination[i];
             }
 
             for (int i = 1; i <= combination.Count - 1; i++)
@@ -26,7 +26,7 @@ namespace Combination.Core
                 var R = CombinationCoefficient.Calculate(totalElements - C[i], combination.Count - i);
                 LI = LI + R;
 
-                while (C[i] < NUM[i])
+                while (C[i] < NUM[i-1])
                 {
                     C[i] = C[i] + 1; //2001
                     R = CombinationCoefficient.Calculate(totalElements - C[i], combination.Count - i);
@@ -36,7 +36,7 @@ namespace Combination.Core
                 LI = LI - R;
             }
 
-            CSN = LI + NUM[combination.Count] - NUM[combination.Count - 1];
+            CSN = LI + NUM[combination.Count - 1] - (combination.Count - 2 < 0 ? 0: NUM[combination.Count - 2]);
 
             return CSN;
         }
