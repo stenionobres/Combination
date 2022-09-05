4356beb954552cd25d9d386b5e1d6d4f9593719e
Stenio Nobres Batista <stenio.nobres@gmail.com>
2022-09-05 18:39:48 -0300

    Implements CSN class and unit tests

--- src/Combination.Core/CSN.cs
+++ src/Combination.Core/CSN.cs
@@ -6,7 +6,42 @@ namespace Combination.Core
     {
         public static int Calculate(int totalElements, List<int> combination)
         {
-            return 1;
+            var V = totalElements;
+            var K = combination.Count;
+            var NUM = new int[K + 1];
+            var C = new int[K + 1];
+            var LI = 0;
+            var CSN = 0;
+            var P1 = K - 1;
+
+            for (int i = 1; i <= K; i++)
+            {
+                NUM[i] = combination[i - 1];
+            }
+
+            for (int i = 1; i <= P1; i++)
+            {
+                C[i] = 0;
+                if (i != 1) C[i] = C[i - 1];
+
+                C[i] = C[i] + 1; //2001
+
+                var R = CombinationCoefficient.Calculate(V - C[i], K - i);
+                LI = LI + R;
+
+                while (C[i] < NUM[i])
+                {
+                    C[i] = C[i] + 1; //2001
+                    R = CombinationCoefficient.Calculate(V - C[i], K - i);
+                    LI = LI + R;
+                }
+
+                LI = LI - R;
+            }
+
+            CSN = LI + NUM[K] - NUM[P1];
+
+            return CSN;
         }
     }
 }
--- src/Combination.Tests/CSNTests.cs
+++ src/Combination.Tests/CSNTests.cs
@@ -8,6 +8,28 @@ namespace Combination.Tests
     public class CSNTests
     {
         [Test]
+        public void GivenTwoElementsAndCombinationWithTwoNumbers_WhenCSNIsCalculated_ThenNumberOneIsGenerated()
+        {
+            var totalElements = 2;
+            var combination = new List<int>() { 1, 2 };
+            var csnExpected = 1;
+            var csnCalculated = CSN.Calculate(totalElements, combination);
+
+            Assert.AreEqual(csnExpected, csnCalculated);
+        }
+
+        [Test]
+        public void GivenTwoElementsAndCombinationWithOneNumber_WhenCSNIsCalculated_ThenNumberTwoIsGenerated()
+        {
+            var totalElements = 2;
+            var combination = new List<int>() { 2 };
+            var csnExpected = 2;
+            var csnCalculated = CSN.Calculate(totalElements, combination);
+
+            Assert.AreEqual(csnExpected, csnCalculated);
+        }
+
+        [Test]
         public void GivenFiveElementsAndCombinationWithThreeNumbers_WhenCSNIsCalculated_ThenNumberOneIsGenerated()
         {
             var totalElements = 5;
@@ -18,5 +40,125 @@ namespace Combination.Tests
             Assert.AreEqual(csnExpected, csnCalculated);
         }
 
+        [Test]
+        public void GivenFiveElementsAndCombinationWithThreeNumbers_WhenCSNIsCalculated_ThenNumberThreeIsGenerated()
+        {
+            var totalElements = 5;
+            var combination = new List<int>() { 1, 2, 5 };
+            var csnExpected = 3;
+            var csnCalculated = CSN.Calculate(totalElements, combination);
+
+            Assert.AreEqual(csnExpected, csnCalculated);
+        }
+
+        [Test]
+        public void GivenEigthElementsAndCombinationWithThreeNumbers_WhenCSNIsCalculated_ThenNumberThreeIsGenerated()
+        {
+            var totalElements = 8;
+            var combination = new List<int>() { 1, 2, 5 };
+            var csnExpected = 3;
+            var csnCalculated = CSN.Calculate(totalElements, combination);
+
+            Assert.AreEqual(csnExpected, csnCalculated);
+        }
+
+        [Test]
+        public void GivenTenElementsAndCombinationWithFiveNumbers_WhenCSNIsCalculated_ThenNumberOneHundredEightyNineIsGenerated()
+        {
+            var totalElements = 10;
+            var combination = new List<int>() { 2, 5, 7, 8, 10 };
+            var csnExpected = 189;
+            var csnCalculated = CSN.Calculate(totalElements, combination);
+
+            Assert.AreEqual(csnExpected, csnCalculated);
+        }
+
+        [Test]
+        public void GivenFifteenElementsAndCombinationWithFiveNumbers_WhenCSNIsCalculated_ThenNumber_1424_IsGenerated()
+        {
+            var totalElements = 15;
+            var combination = new List<int>() { 2, 5, 7, 8, 10 };
+            var csnExpected = 1424;
+            var csnCalculated = CSN.Calculate(totalElements, combination);
+
+            Assert.AreEqual(csnExpected, csnCalculated);
+        }
+
+        [Test]
+        public void GivenFifteenElementsAndCombinationWithFiveNumbers_WhenCSNIsCalculated_ThenNumberOneIsGenerated()
+        {
+            var totalElements = 15;
+            var combination = new List<int>() { 1, 2, 3, 4, 5 };
+            var csnExpected = 1;
+            var csnCalculated = CSN.Calculate(totalElements, combination);
+
+            Assert.AreEqual(csnExpected, csnCalculated);
+        }
+
+        [Test]
+        public void GivenFifteenElementsAndCombinationWithFiveNumbers_WhenCSNIsCalculated_ThenNumber_3003_IsGenerated()
+        {
+            var totalElements = 15;
+            var combination = new List<int>() { 11, 12, 13, 14, 15 };
+            var csnExpected = 3003;
+            var csnCalculated = CSN.Calculate(totalElements, combination);
+
+            Assert.AreEqual(csnExpected, csnCalculated);
+        }
+
+        [Test]
+        public void GivenSixtyElementsAndCombinationWithSixNumbers_WhenCSNIsCalculated_ThenNumberFiveIsGenerated()
+        {
+            var totalElements = 60;
+            var combination = new List<int>() { 1, 2, 3, 4, 5, 10 };
+            var csnExpected = 5;
+            var csnCalculated = CSN.Calculate(totalElements, combination);
+
+            Assert.AreEqual(csnExpected, csnCalculated);
+        }
+
+        [Test]
+        public void GivenTwentyFiveElementsAndCombinationWithFifteenNumbers_WhenCSNIsCalculated_ThenNumberOneIsGenerated()
+        {
+            var totalElements = 25;
+            var combination = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            var csnExpected = 1;
+            var csnCalculated = CSN.Calculate(totalElements, combination);
+
+            Assert.AreEqual(csnExpected, csnCalculated);
+        }
+
+        [Test]
+        public void GivenTwentyFiveElementsAndCombinationWithFifteenNumbers_WhenCSNIsCalculated_ThenNumber_737432_IsGenerated()
+        {
+            var totalElements = 25;
+            var combination = new List<int>() { 1, 2, 4, 5, 6, 11, 13, 14, 15, 16, 20, 21, 23, 24, 25 };
+            var csnExpected = 737432;
+            var csnCalculated = CSN.Calculate(totalElements, combination);
+
+            Assert.AreEqual(csnExpected, csnCalculated);
+        }
+
+        [Test]
+        public void GivenTwentyFiveElementsAndCombinationWithFifteenNumbers_WhenCSNIsCalculated_ThenNumber_940577_IsGenerated()
+        {
+            var totalElements = 25;
+            var combination = new List<int>() { 1, 2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+            var csnExpected = 940577;
+            var csnCalculated = CSN.Calculate(totalElements, combination);
+
+            Assert.AreEqual(csnExpected, csnCalculated);
+        }
+
+        [Test]
+        public void GivenTwentyFiveElementsAndCombinationWithFifteenNumbers_WhenCSNIsCalculated_ThenNumber_3268760_IsGenerated()
+        {
+            var totalElements = 25;
+            var combination = new List<int>() { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+            var csnExpected = 3268760;
+            var csnCalculated = CSN.Calculate(totalElements, combination);
+
+            Assert.AreEqual(csnExpected, csnCalculated);
+        }
     }
 }
