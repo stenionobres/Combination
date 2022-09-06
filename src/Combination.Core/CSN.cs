8c7124aacabc85acb4eaefa403f5256b385034b2
Stenio Nobres Batista <stenio.nobres@gmail.com>
2022-09-06 20:30:15 -0300

    Adds new algorithm for CSN class
    This algorithm works for all scenarios inclusive with Sixty numbers. The previous algorithm was save in the class.

--- src/Combination.Core/CSN.cs
+++ src/Combination.Core/CSN.cs
@@ -6,26 +6,45 @@ namespace Combination.Core
     {
         public static int Calculate(int totalElements, List<int> combination)
         {
-            var R = 0;
-            var LI = 0;
+            var r = CombinationCoefficient.Calculate(totalElements, combination.Count);
 
-            for (int i = 0, j = 0; i < combination.Count - 1; i++)
+            for (int i = 0; i < combination.Count; i++)
             {
-                while (j < combination[i])
-                {
-                    j++;
-                    R = CombinationCoefficient.Calculate(totalElements - j, combination.Count - (i + 1));
-                    LI = LI + R;
-                }
-
-                LI = LI - R;
+                if (totalElements - combination[i] < combination.Count - i) continue;
+                r = r - CombinationCoefficient.Calculate(totalElements - combination[i], combination.Count - i);
             }
 
-            var CSN = LI + combination[combination.Count - 1];
-
-            if (combination.Count - 2 >= 0) CSN = CSN - combination[combination.Count - 2];
-
-            return CSN;
+            return r;
         }
     }
+
+    /*
+     * 
+     * 
+        // Algoritmo refatorado a partir do algoritmo citado no link https://saliu.com/bbs/messages/348.html
+        // Esse algoritmo não funciona para cenários com 60 números e combinações com 6 números
+        var R = 0;
+        var LI = 0;
+
+        for (int i = 0, j = 0; i < combination.Count - 1; i++)
+        {
+            while (j < combination[i])
+            {
+                j++;
+                R = CombinationCoefficient.Calculate(totalElements - j, combination.Count - (i + 1));
+                LI = LI + R;
+            }
+
+            LI = LI - R;
+        }
+
+        var CSN = LI + combination[combination.Count - 1];
+
+        if (combination.Count - 2 >= 0) CSN = CSN - combination[combination.Count - 2];
+
+        return CSN;
+
+    *
+    *
+    */
 }
