c561a2d8ef40fed034d69f6d2933d65eacfbbc4a
Stenio Nobres Batista <stenio.nobres@gmail.com>
2022-09-11 16:01:09 -0300

    Adds initial design and unit tests for Combination class

---
+++ src/Combination.Core/Combination.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Combination.Core
+{
+    public class Combination
+    {
+        public int TotalElements { get; }
+        public int CombinationSize { get; }
+        public int CSN { get; }
+        public List<int> Elements { get; private set; }
+
+        public Combination(int totalElements, int combinationSize)
+        {
+            TotalElements = totalElements;
+            CombinationSize = combinationSize;
+        }
+
+        public void buildWith(List<int> elements)
+        {
+            Elements = elements;
+        }
+    }
+}
---
+++ src/Combination.Tests/CombinationTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Combination.Tests
+{
+    [TestFixture]
+    public class CombinationTests
+    {
+        [Test]
+        public void GivenTotalElementsLessThanCombinationSize_WhenCSNIsCalculated_ThenNumberZeroIsGenerated()
+        {
+            var csnExpected = 0;
+            var combination = new Core.Combination(totalElements: 0, combinationSize: 2);
+            combination.buildWith(elements: new List<int> { 1, 2 });
+
+            Assert.AreEqual(csnExpected, combination.CSN);
+        }
+    }
+}
