542c705348c7caa45442b4ee97bf7d39904d45de
Stenio Nobres Batista <stenio.nobres@gmail.com>
2022-09-11 18:54:42 -0300

    Save old algorithm

--- src/Combination.Core/Combination.cs
+++ src/Combination.Core/Combination.cs
@@ -71,3 +71,33 @@ namespace Combination.Core
         }
     }
 }
+
+/*
+ * 
+ * 
+    // Algoritmo refatorado a partir do algoritmo citado no link https://saliu.com/bbs/messages/348.html
+    // Esse algoritmo não funciona para cenários com 60 números e combinações com 6 números
+    var R = 0;
+    var LI = 0;
+
+    for (int i = 0, j = 0; i < combination.Count - 1; i++)
+    {
+        while (j < combination[i])
+        {
+            j++;
+            R = CombinationCoefficient.Calculate(totalElements - j, combination.Count - (i + 1));
+            LI = LI + R;
+        }
+
+        LI = LI - R;
+    }
+
+    var CSN = LI + combination[combination.Count - 1];
+
+    if (combination.Count - 2 >= 0) CSN = CSN - combination[combination.Count - 2];
+
+    return CSN;
+
+*
+*
+*/
