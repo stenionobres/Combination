b075d1e5fda024744b0a7e95442e71f55b1c7faa
Stenio Nobres Batista <stenio.nobres@gmail.com>
2022-09-28 20:13:28 -0300

    Adds results and new Program class

--- src/Combination.App/Program.cs
+++ src/Combination.App/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Linq;
+using Combination.Core;
+using System.Collections.Generic;
 
 namespace Combination.App
 {
@@ -6,7 +8,26 @@ namespace Combination.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lotofacilResults = new LotofacilResults("../../../../../resources/", "resultadosltf.txt");
+            var groupingCSN = new GroupingCSN(totalElements: 25, combinationSize: 15, groupSize: 100000);
+            var groupingCSNCounter = new Dictionary<int, int>();
+
+            foreach (var lotofacilResult in lotofacilResults.Results)
+            {
+                var combination = new Core.Combination(totalElements: 25, combinationSize: 15);
+                combination.buildWith(lotofacilResult.Value);
+
+                var csnGroup = groupingCSN.GetGroup(combination.CSN);
+
+                if (groupingCSNCounter.ContainsKey(csnGroup) == false)
+                {
+                    groupingCSNCounter.Add(csnGroup, 0);
+                }
+
+                groupingCSNCounter[csnGroup]++;
+            }
+
+            var groupingCSNCounterOrdered = groupingCSNCounter.OrderByDescending(g => g.Value);
         }
     }
 }
---
+++ src/Combination.Tests/GroupingCSNTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Combination.Core;
+
+namespace Combination.Tests
+{
+    [TestFixture]
+    public class GroupingCSNTests
+    {
+        [Test]
+        public void TestCSN()
+        {
+            var groupingCSN = new GroupingCSN(totalElements: 10, combinationSize: 3, groupSize: 20);
+            
+
+        }
+
+        [Test]
+        public void TestLoadLotofacilResults()
+        {
+            var lotofacilResults = new LotofacilResults("../../../../../resources/", "resultadosltf.txt");
+
+
+        }
+    }
+}
